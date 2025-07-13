88522023e39a931b5a5a8d58cf637f12b2315c7a
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-07-13 23:02:47 +0300

    s5e8825-common: vendorsetup: Add source-wide patch for NFC

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,9 @@ fi
 if [ -d "hardware/samsung/AdvancedDisplay" ]; then
     rm -rf "hardware/samsung/AdvancedDisplay"
 fi
+
+if [ -d "system/nfc" ] && ! grep "nfa_t4tnfcee_is_config" system/nfc/src/nfa/include/nfa_nfcee_int.h > /dev/null; then
+  cd system/nfc
+  git fetch https://github.com/LineageOS/android_system_nfc refs/changes/55/423355/1 && git cherry-pick FETCH_HEAD
+  cd -
+fi
