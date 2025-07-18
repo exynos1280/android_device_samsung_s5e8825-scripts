76b1071f18df77d07736b075252b14720fbf1e02
Flopster101 <nahuelgomez329@gmail.com>
2025-07-18 03:25:45 -0300

    s5e8825-common: Drop incompatible Samsung features
    Doze and AdvancedDisplay require LOS stuff.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+if [ -d "hardware/samsung/doze" ]; then
+    rm -rf "hardware/samsung/doze"
+fi
+
+if [ -d "hardware/samsung/AdvancedDisplay" ]; then
+    rm -rf "hardware/samsung/AdvancedDisplay"
+fi
