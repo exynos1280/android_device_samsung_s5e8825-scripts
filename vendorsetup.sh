de6b4fb9ff857b8c849bf0f945d123f40893dcac
Flopster101 <nahuelgomez329@gmail.com>
2025-08-31 03:51:41 -0300

    s5e8825-common: Update telephony patch
    It's a single commit now.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,16 +2,9 @@ if ! grep -q "smscPduToPhoneNumber" "frameworks/opt/telephony/src/java/com/andro
   echo "Applying Samsung SMSC patches to Telephony..."
 
   pushd "frameworks/opt/telephony" > /dev/null
-  git fetch https://github.com/exynos1280/platform_frameworks_opt_telephony android-15.0.0_r26-td
+  git fetch https://github.com/exynos1280/platform_frameworks_opt_telephony android-15.0.0_r26
 
-  # 1. "ignore invalid SMSC" patch from PHH
-  git cherry-pick 0fdaaba9c88cbc3d8c9c5288d6d2820fd6bd344e
-
-  # 2. "actually parse it" patch from PHH
-  git cherry-pick 9de26ca6b0fcbbb8bc27ca75a57697a7724f797d
-
-  # 3. "Convert PDU to phone number" patch
-  git cherry-pick 4bc77f13f09ca37c034adea8d4c401f2df5cb01d
+  git cherry-pick 28fe40db08282f5a9cacccbacd1447fa6998c03c
 
   popd > /dev/null
   echo "Telephony patches applied successfully."
