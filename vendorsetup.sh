4eea3f124f59862d93b4d6e2326595e144934b39
Majaahh <maja.blaszczak@proton.me>
2025-09-01 19:32:38 +0200

    s5e8825-common: burp
    Change-Id: I040535976cac37ca6a26eaa74c60384629156f67
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,11 @@
 if ! grep -q "smscPduToPhoneNumber" "frameworks/opt/telephony/src/java/com/android/internal/telephony/RadioResponse.java"; then
   echo "Applying Samsung SMSC patches to Telephony..."
 
-  pushd "frameworks/opt/telephony" > /dev/null
-  git fetch https://github.com/exynos1280/platform_frameworks_opt_telephony android-15.0.0_r26
-
-  git cherry-pick 28fe40db08282f5a9cacccbacd1447fa6998c03c
+  (
+      cd "frameworks/opt/telephony"
+      git fetch https://github.com/exynos1280/platform_frameworks_opt_telephony android-15.0.0_r26
+      git cherry-pick 28fe40db08282f5a9cacccbacd1447fa6998c03c
+  )
 
   popd > /dev/null
   echo "Telephony patches applied successfully."
