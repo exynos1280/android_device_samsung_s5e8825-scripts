90902b59dfa32382c1422e9a0a165fb1a35add4d
Flopster101 <nahuelgomez329@gmail.com>
2025-09-27 19:43:04 -0300

    s5e8825-common: Apply patch to fix a crash in Quickspace
    09-27 19:22:08.609 11550 11550 E AndroidRuntime: FATAL EXCEPTION: main
    09-27 19:22:08.609 11550 11550 E AndroidRuntime: Process: com.android.launcher3, PID: 11550
    09-27 19:22:08.609 11550 11550 E AndroidRuntime: android.util.AndroidRuntimeException: Calling startActivity() from outside of an Activity context requires the FLAG_ACTIVITY_NEW_TASK flag. Is this really what you want?
    09-27 19:22:08.609 11550 11550 E AndroidRuntime:        at android.app.ContextImpl.startActivity(ContextImpl.java:1156)
    09-27 19:22:08.609 11550 11550 E AndroidRuntime:        at android.app.ContextImpl.startActivity(ContextImpl.java:1132)
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,3 +20,13 @@ if ! grep -q "build_maintainer" "packages/apps/Settings/res/values/cm_strings.xm
   )
   echo "Settings maintainer patch applied successfully."
 fi
+
+if ! grep -q "FLAG_ACTIVITY_NEW_TASK" "packages/apps/Launcher3/src/com/android/launcher3/quickspace/QuickEventsController.java"; then
+  echo "Applying Launcher3 Quickspace crash fix..."
+
+  (
+    cd "packages/apps/Launcher3"
+    git am -3 "$SCRIPTS_ROOT/patches/0001-Launcher3-Fix-QuickSpace-crash-by-adding-FLAG_ACTIVI.patch"
+  )
+  echo "Launcher3 Quickspace crash fix applied successfully."
+fi
