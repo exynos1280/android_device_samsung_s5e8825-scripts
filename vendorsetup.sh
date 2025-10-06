125c0a8a743448b467d81f0286f7f1d2bb3b374d
Flopster101 <nahuelgomez329@gmail.com>
2025-10-06 03:49:22 -0300

    s5e8825-common: Move telephony patch to repo
    No need to fetch.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,11 @@
+SCRIPTS_ROOT=$(realpath device/samsung/s5e8825-scripts)
+
 if ! grep -q "smscPduToPhoneNumber" "frameworks/opt/telephony/src/java/com/android/internal/telephony/RadioResponse.java"; then
   echo "Applying Samsung SMSC patches to Telephony..."
 
   (
       cd "frameworks/opt/telephony"
-      git fetch https://github.com/exynos1280/platform_frameworks_opt_telephony android-15.0.0_r26
-      git cherry-pick 28fe40db08282f5a9cacccbacd1447fa6998c03c
+      git am -3 "$SCRIPTS_ROOT/patches/telephony/0001-telephony-Fix-various-Samsung-SMSC-formatting-issues.patch"
   )
   echo "Telephony patches applied successfully."
 fi
