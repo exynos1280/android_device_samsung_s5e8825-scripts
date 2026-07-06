b70f4a075b4119ab02ad34d80990dc2e795c0a83
Flopster101 <nahuelgomez329@gmail.com>
2026-07-06 19:54:54 -0300

    s5e8825-common: Remove telephony patch
    No longer needed after [1].
    
    * https://github.com/exynos1280/android_device_samsung_s5e8825-common/commit/65dddcb2f1a9d60440068c28f335b89376fb754e
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,5 @@
 SCRIPTS_ROOT=$(realpath device/samsung/s5e8825-scripts)
 
-if ! grep -q "smscPduToPhoneNumber" "frameworks/opt/telephony/src/java/com/android/internal/telephony/RadioResponse.java"; then
-  echo "Applying Samsung SMSC patches to Telephony..."
-
-  (
-      cd "frameworks/opt/telephony"
-      git am -3 "$SCRIPTS_ROOT/patches/telephony/0001-telephony-Fix-various-Samsung-SMSC-formatting-issues.patch"
-  )
-  echo "Telephony patches applied successfully."
-fi
-
 if ! grep -q "setCurrentUsbFunctions mass_storage" "hardware/samsung/aidl/usb/gadget/UsbGadget.cpp"; then
   echo "Applying Samsung mass storage patch..."
 
